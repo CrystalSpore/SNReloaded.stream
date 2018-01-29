1d6f7a8d24f111f553ff459f6812322fcbdb7b6b
CrystalSpore <sporeluc@gmail.com>
2018-01-29 12:29:36 -0500

    Added Linux pages skeleton AND updated scripts to work from root directory instead of going up X pages

--- files/script/footer.js
+++ files/script/footer.js
@@ -1,1 +1,2 @@
+document.getElementsByTagName("main")[0].innerHTML += '<div id="footerBuffer" style="height:60px;"></div>'
 document.write('<div class="footer"> <hr /> <p> &copy; 2017<script>new Date().getFullYear()>2017&&document.write("-"+new Date().getFullYear());</script>, \ SNReloaded (Crystal Fletcher). </p> </div>');
