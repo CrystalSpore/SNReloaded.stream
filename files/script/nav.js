29978261dad6d465c2c889d8a3aa2577c316e985
CrystalSpore <sporeluc@gmail.com>
2018-04-20 01:47:03 -0400

    Mobile formatting updates

--- files/script/nav.js
+++ files/script/nav.js
@@ -1,1 +1,5 @@
-document.write('<header><a href="/index.html"><img src="/files/images/SNR-banner.png" width="100%" height="100%" alt=""/></a></header><nav>| <a href="/">HOME</a> | <a href="/about/">ABOUT</a> | <a href="/github/">GITHUB</a> | <a href="/twitch/">TWITCH</a> |<br />| <a href="/schedule/">SCHEDULE</a> | <a href="/tips/">TIP JAR</a> | <a href="/credits/">CREDITS</a> |</nav><hr />');
+if( /Android|WebOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    document.write('<header><a href="/index.html"><img src="/files/images/SNR-banner.png" width="100%" height="100%" alt=""/></a></header><nav>| <a href="/">HOME</a> | <a href="/about/">ABOUT</a> |<br />| <a href="/github/">GITHUB</a> | <a href="/twitch/">TWITCH</a> |<br />| <a href="/schedule/">SCHEDULE</a> | <a href="/tips/">TIP JAR</a> |<br />| <a href="/credits/">CREDITS</a> |</nav><hr />');
+} else {
+    document.write('<header><a href="/index.html"><img src="/files/images/SNR-banner.png" width="100%" height="100%" alt=""/></a></header><nav>| <a href="/">HOME</a> | <a href="/about/">ABOUT</a> | <a href="/github/">GITHUB</a> | <a href="/twitch/">TWITCH</a> |<br />| <a href="/schedule/">SCHEDULE</a> | <a href="/tips/">TIP JAR</a> | <a href="/credits/">CREDITS</a> |</nav><hr />');
+}
