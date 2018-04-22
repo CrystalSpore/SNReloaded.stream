762d0e3f478a7085d40d17ec7ffa7265e1118d63
CrystalSpore <sporeluc@gmail.com>
2018-04-22 16:00:26 -0400

    fixed ./tips for mobile

--- files/script/nav.js
+++ files/script/nav.js
@@ -1,5 +1,5 @@
 if( /Android|WebOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    document.write('<header><a href="/index.html"><img src="/files/images/SNR-banner.png" width="100%" height="100%" alt=""/></a></header><nav>| <a href="/">HOME</a> | <a href="/about/">ABOUT</a> |<br />| <a href="/github/">GITHUB</a> | <a href="/twitch/">TWITCH</a> |<br />| <a href="/schedule/">SCHEDULE</a> | <a href="/tips/">TIP JAR</a> |<br />| <a href="/credits/">CREDITS</a> |</nav><hr />');
+    document.write('<header><a href="/index.html"><img src="/files/images/SNR-banner.png" width="100%" height="100%" alt=""/></a></header><nav>| <a href="/">HOME</a> | <a href="/about/">ABOUT</a> |<br />| <a href="/github/">GITHUB</a> | <a href="/schedule/">SCHEDULE</a> |<br />|  <a href="/tips/">TIP JAR</a> | <a href="/credits/">CREDITS</a> |</nav><hr />');
 } else {
     document.write('<header><a href="/index.html"><img src="/files/images/SNR-banner.png" width="100%" height="100%" alt=""/></a></header><nav>| <a href="/">HOME</a> | <a href="/about/">ABOUT</a> | <a href="/github/">GITHUB</a> | <a href="/twitch/">TWITCH</a> |<br />| <a href="/schedule/">SCHEDULE</a> | <a href="/tips/">TIP JAR</a> | <a href="/credits/">CREDITS</a> |</nav><hr />');
 }
